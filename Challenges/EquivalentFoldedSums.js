9c01ce237660385314154eb86ebd17e0a5aeafd2
Owen Yoshishige <owenyoshishige@gmail.com>
2022-05-26 13:33:18 -0700

    create equivalantFoldedSums problem

---
+++ Challenges/EquivalentFoldedSums.js
@@ -0,0 +1,21 @@
+/*
+You are given a list of integers nums of even length. Consider an operation where you pick any number in nums and update it with a value between [1, max(nums)].
+
+Return the minimum number of operations required such that for every i, nums[i] + nums[n - 1 - i] equals to the same number.
+
+Constraints
+n ≤ 100,000 where n is the length of nums
+1 ≤ nums[i] ≤ 10,000
+
+Input
+nums = [1, 4, 3, 5]
+
+Output
+1
+*/
+
+const equivalentFoldedSums = () => {
+  //To-do
+};
+
+module.exports = equivalentFoldedSums;
