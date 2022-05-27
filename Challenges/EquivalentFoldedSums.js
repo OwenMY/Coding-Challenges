645963949dfe1a29567c8405e5af72ca2c9fa62f
Owen Yoshishige <owenyoshishige@gmail.com>
2022-05-27 14:55:29 -0700

    attempt Equivalent Folded Sums

--- Challenges/EquivalentFoldedSums.js
+++ Challenges/EquivalentFoldedSums.js
@@ -12,29 +12,94 @@ nums = [1, 4, 3, 5]
 
 Output
 1
+What number can be created in common?
+
+The sum of each pair can be made equal to a given value in at-most one update if this value lies in a particular range. However, if the value lies outside this range then we require two updates.
+Can you find this range?
+
+To calculate this range, think about the maximum amount by which you can increment and decrement the sum of a particular pair by changing only one of the two values.
+
+Suppose you know this range, let it be [lo, hi]. Can you think of an optimal approach to find the value that requires the least number of changes to make all pair sums equal to this value? Think in terms of Prefix Sums.
 */
+
+//Sum has to land in a particular range
+//
 
 const equivalentFoldedSums = (nums) => {
   if (nums.length === 2) {
     return 0;
   }
 
-  let standard = nums[0] + nums[nums.length - 1];
-  let minimumOps = 0;
+  let lowestNum = Math.min(...nums);
+  let highestNum = Math.max(...nums);
 
-  for (let i = 1; i < nums.length / 2; i++) {
-    if (nums[i] + nums[nums.length - 1 - i] !== standard) {
-      if (nums[i] !== 0) {
-        nums[i]--;
-      } else if (nums[nums.length - 1 - i] !== 0) {
-        nums[nums.length - 1 - i]--;
+  let sums = [];
+  let lowCandidates = [];
+  let highCandidates = [];
+
+  for (let i = 0; i < nums.length / 2; i++) {
+    let sum = nums[i] + nums[nums.length - 1 - i];
+    lowCandidates.push([nums[i] + lowestNum, nums[nums.length - 1 - i] + lowestNum]);
+    highCandidates.push([nums[i] + highestNum, nums[nums.length - 1 - i] + highestNum]);
+    sums.push(sum);
+  }
+
+  if (sums.every((sum) => sum === sums[0])) {
+    return 0;
+  }
+
+  const getLeastCommonFactor = (arr) => {
+    let lowNumbers = [];
+
+    arr.forEach((numSet) => lowNumbers.push(...numSet));
+    let lowRange = [Math.min(...lowNumbers), Math.max(...lowNumbers)];
+
+    for (let i = lowRange[0]; i < lowRange[1]; i++) {
+      if (arr.every((numSet) => numSet.includes(i))) {
+        return i;
       }
-      i-= 2;
-      minimumOps++;
+    }
+
+    return Math.max(...lowNumbers);
+  };
+
+  const getGreatestCommonFactor = (arr) => {
+    let highNumbers = [];
+
+    arr.forEach((numSet) => highNumbers.push(...numSet));
+    let highRange = [Math.min(...highNumbers), Math.max(...highNumbers)];
+
+    for (let i = highRange[0]; i < highRange[1]; i++) {
+      if (arr.every((numSet) => numSet.includes(i))) {
+        return i;
+      }
+    }
+
+    return Math.min(...highNumbers);
+  };
+
+  let range = [getLeastCommonFactor(lowCandidates), getGreatestCommonFactor(highCandidates)];
+  let standard = null;
+
+  for (let sum of sums) {
+    if (sum >= range[0] && sum <= range[1]) {
+      standard = sum;
+      break;
     }
   }
 
-  return minimumOps;
+  let ops = 0;
+  if (standard === null) {
+    standard = range[0];
+  }
+
+  for (let sum of sums) {
+    if (standard !== sum) {
+      ops++;
+    }
+  }
+
+  return ops;
 };
 
 module.exports = equivalentFoldedSums;
--- __tests__/equivalentFoldedSums.test.js
+++ __tests__/equivalentFoldedSums.test.js
@@ -5,6 +5,14 @@ describe('Equivalent Folded Sums', () => {
     expect(equivalentFoldedSums([1, 4, 3, 5])).toBe(1);
     expect(equivalentFoldedSums([1, 2, 2, 1])).toBe(2);
     expect(equivalentFoldedSums([1, 1, 2, 3])).toBe(1);
+    expect(equivalentFoldedSums([1, 1, 2, 4])).toBe(1);
+    expect(equivalentFoldedSums([1, 1, 2, 1])).toBe(1);
+    expect(equivalentFoldedSums([1, 1, 2, 2])).toBe(0);
+    expect(equivalentFoldedSums([1, 2, 1, 1])).toBe(1);
+    expect(equivalentFoldedSums([3, 1, 2, 1])).toBe(1);
+    expect(equivalentFoldedSums([1, 3, 3, 2])).toBe(2);
+    expect(equivalentFoldedSums([1, 2, 3, 2])).toBe(1);
+    expect(equivalentFoldedSums([1, 2, 4, 3])).toBe(1);
   });
 
   test('Should return 0 when given an array with 2 elements', () => {
