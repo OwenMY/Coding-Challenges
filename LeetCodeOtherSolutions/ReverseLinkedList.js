8cc8515e525786f251fac9eb0378a531df9d8b21
Owen Yoshishige <owenyoshishige@gmail.com>
2022-06-02 23:10:08 -0700

    create tests and starter code for ReversedLinkedList

--- LeetCodeOtherSolutions/ReverseLinkedList.js
+++ LeetCodeOtherSolutions/ReverseLinkedList.js
@@ -1,4 +1,39 @@
 /*
-Refer to https://github.com/OwenMY/LeetCode/tree/master/206-reverse-linked-list for prompt.  This problem is an attempt to solve this problem iteratively.
+Refer to https://github.com/OwenMY/LeetCode/tree/master/206-reverse-linked-list for prompt. This is an attempt to solve this problem iteratively.
 */
+class LinkedList {
+  constructor(values) {
+    this.head = linkNodes(values);
+  };
+};
 
+class ListNode {
+  constructor(val) {
+    this.next = null;
+    this.val = val;
+  }
+}
+
+const linkNodes = (values) => {
+  let topNode = new ListNode(values[0]);
+  let currentNode = topNode;
+
+  for (let i = 1; i < values.length; i++) {
+    let newNode = new ListNode(values[i]);
+    currentNode.next = newNode;
+    currentNode = newNode;
+  }
+
+  return topNode;
+};
+
+const reverseLinkedList = (head) => {
+  //To-do
+};
+
+let linkedList1 = new LinkedList([1, 2, 3, 4, 5]);
+
+module.exports = {
+  linkedList1,
+  reverseLinkedList
+};
---
+++ __tests__/ReverseLinkedList.test.js
@@ -0,0 +1,7 @@
+const { linkedList1, reverseLinkedList } = require('./../LeetCodeOtherSolutions/ReverseLinkedList.js');
+
+describe('Reverse linked list', () => {
+  test('Should be a linked list', () => {
+    expect(linkedList1).toBe(linkedList1);
+  });
+});
