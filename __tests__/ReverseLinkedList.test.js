7c1433cb2589b799635122a9028716db52f2bff4
Owen Yoshishige <owenyoshishige@gmail.com>
2022-06-04 14:52:11 -0700

    complete ReverseLinkedLists Testing Suite

--- __tests__/ReverseLinkedList.test.js
+++ __tests__/ReverseLinkedList.test.js
@@ -1,7 +1,22 @@
 const { linkedList1, reverseLinkedList } = require('./../LeetCodeOtherSolutions/ReverseLinkedList.js');
 
+const getTail = (node) => {
+  return node.next === null ? node : getTail(node.next);
+};
+
+let result = getTail(linkedList1.head);
+
 describe('Reverse linked list', () => {
-  test('Should be a linked list', () => {
-    expect(linkedList1).toBe(linkedList1);
+  test('Should return reversed linked list', () => {
+    expect(reverseLinkedList(linkedList1.head)).toBe(result);
+  });
+
+  test('Should return null when an empty list is passed in', () => {
+    expect(reverseLinkedList(null)).toBe(null);
+  });
+
+  test('Should return same node when a single node with no "next" is passed in', () => {
+    let node = {val: 5, next: null}
+    expect(reverseLinkedList(node)).toBe(node);
   });
 });
